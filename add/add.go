c7f124946634023cfc62a9fb0262d2b10226577c
Nailah Brown <nailah@Nailahs-MacBook-Air.local>
2025-02-25 21:59:05 -0500

    Finalizing everything

--- add/add.go
+++ add/add.go
@@ -18,10 +18,26 @@ func Add(a, b int) int {
 
 //exercise 2
 
-package add
+/*package add
 
 // Add takes two integers and returns their sum.
 // For more details, visit: https://www.mathsisfun.com/numbers/addition.html.
 func Add(a, b int) int {
+	return a + b
+}
+*/
+
+// Package add provides basic mathematical operations with support for generics.
+package add
+
+import "golang.org/x/exp/constraints"
+
+// Number is a generic type constraint that allows both integers and floats.
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add takes two numbers (integers or floats) and returns their sum.
+func Add[T Number](a, b T) T {
 	return a + b
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nini2255/chap10
 
 go 1.23.5
+
+require golang.org/x/exp v0.0.0-20250218142911-aa4b98e5adaa // indirect
