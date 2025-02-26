e347d92f55a1d12adf90bf22ced93af90a631691
Nailah Brown <nailah@Nailahs-MacBook-Air.local>
2025-02-25 21:35:30 -0500

    Added add function

---
+++ add/add.go
@@ -0,0 +1,16 @@
+//exercise 1
+
+package add
+
+// Add takes two integers and returns their sum.
+func Add(a, b int) int {
+	return a + b
+}
+
+//to commit changes:
+
+//git add .
+//git commit -m "Added Add function"
+//git tag v1.0.0
+//git push origin main
+//git push origin v1.0.0
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nini2255/chap10
+
+go 1.23.5
